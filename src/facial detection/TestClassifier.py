b65a5b088a7033f940db7335b21062950147528d
gabecasciano <gabecasciano@gmail.com>
2020-07-08 13:36:28 -0400

    added gstreamer

--- src/facial detection/TestClassifier.py
+++ src/facial detection/TestClassifier.py
@@ -6,11 +6,25 @@ sys.path.append('.')
 
 from Recognition.Classifier import Classifier
 
+def get_jetson_gstreamer_source(capture_width=1280, capture_height=720, display_width=640, display_height=480,
+                                framerate=30, flip_method=2):
+    """
+    Return an OpenCV-compatible video source description that uses gstreamer to capture video from the camera on a Jetson Nano
+    """
+    return (
+            f'nvarguscamerasrc ! video/x-raw(memory:NVMM), ' +
+            f'width=(int){capture_width}, height=(int){capture_height}, ' +
+            f'format=(string)NV12, framerate=(fraction){framerate}/1 ! ' +
+            f'nvvidconv flip-method={flip_method} ! ' +
+            f'video/x-raw, width=(int){display_width}, height=(int){display_height}, format=(string)BGRx ! ' +
+            'videoconvert ! video/x-raw, format=(string)BGR ! appsink')
+
+
 
 class gui:
     def __init__(self, tk: Tk):
         self.tk = tk
-        self.vs = cv2.VideoCapture(0)
+        self.vs = cv2.VideoCapture(get_jetson_gstreamer_source())
 
         self.classifier = Classifier()
         self.classifier.initModel()
