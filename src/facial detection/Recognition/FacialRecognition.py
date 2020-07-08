99f8a403515d6320bca3f89e5424d8f165c97d93
gabe <gabecasciano@gmail.com>
2020-07-08 13:21:11 -0400

    stuff

--- src/facial detection/Recognition/FacialRecognition.py
+++ src/facial detection/Recognition/FacialRecognition.py
@@ -19,10 +19,9 @@ class Recognizer:
         return self.cropped
 
     def findandcrop(self, img):
-        cascade = cv2.CascadeClassifier('C:\\Users\\Gabe Casciano\\Documents\\opencv\\build\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+        cascade = cv2.CascadeClassifier('~/opencv/data/haarcascade/haarcascade_frontalface_default.xml')
         tmp = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         rect = cascade.detectMultiScale(tmp, scaleFactor=1.1, minNeighbors=5)
-
         for (x, y, h, w) in rect:
             self.cropped = img[y:y + h, x:x + w]
 
